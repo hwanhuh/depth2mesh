500e8f61fbf92933219239552c0e50f4ff64b5b8
HwanHeo <gjghks950@naver.com>
2024-06-28 16:03:12 +0900

    Update depth_to_textured_mesh.py
    add png input 
    add ptc extraction

--- depth_to_textured_mesh.py
+++ depth_to_textured_mesh.py
@@ -15,13 +15,15 @@ def move_bottom_right(mask): return np.pad(mask,((1,0),(1,0)),'constant',constan
 
 class MeshMaker:
     def __init__(self, 
-                 depth_path, 
-                 image_path,
+                 depth_path: str, 
+                 image_path: str,
+                 is_png: bool=False,
                  scale=None,
                  ):
         self.depth_path = depth_path 
         self.image_path = image_path
         self.scale = scale
+        self.is_png = is_png 
 
     def construct_facets_from(self, mask):
         idx = np.zeros_like(mask, dtype=int)
@@ -54,15 +56,26 @@ class MeshMaker:
         vertices[..., 2] = depths
 
         return vertices
+    
+    def load_depth(self):
+        if self.is_png:
+            depth = cv2.imread(self.depth_path, cv2.IMREAD_GRAYSCALE)
+            depth = 1 - depth
+        else:
+            depth = np.load(self.depth_path)
+        return depth
 
-    def get_mesh_from_depth(self):
-        self.depth_map = np.load(self.depth_path)
+    def get_mesh_from_depth(self, textures):
+        self.depth_map = self.load_depth()
         if self.scale is None:
-            self.scale = np.sqrt(self.depth_map.shape[0] * self.depth_map.shape[1])
+            self.scale = np.sqrt(self.depth_map.shape[0] * self.depth_map.shape[1]) / 500
         print('[Info] Make pointclouds from depth maps')
         vertices = self.map_depth_map_to_point_clouds((1 - self.depth_map) * self.scale)
 
         print('[Info] Constructing quadface facets for all pixels')
+        # if textures.shape[-1] == 4:
+        #     facets = self.construct_facets_from(textures[:, :, -1].astype(bool))
+        # else:
         facets = self.construct_facets_from(np.ones(self.depth_map.shape).astype(bool))
 
         faces = []
@@ -77,10 +90,14 @@ class MeshMaker:
         return vertices, faces
 
     def make_textured_mesh(self):
-        vertices, faces = self.get_mesh_from_depth()
-        textures = cv2.imread(self.image_path)
+        textures = cv2.imread(self.image_path, cv2.IMREAD_UNCHANGED)
+        mask = textures[:, :, -1]
+        kernel = np.ones((5, 5), np.uint8)
+        eroded_mask = cv2.erode(mask, kernel, iterations=1).astype(bool)
+        
+        vertices, faces = self.get_mesh_from_depth(textures)
         assert textures.shape[:2] == self.depth_map.shape
-        textures = cv2.cvtColor(textures, cv2.COLOR_BGR2RGB)
+        textures = cv2.cvtColor(textures[:, :, :3], cv2.COLOR_BGR2RGB)
         textures = textures / 255
         
         print('[Info] Making Textured-Mesh')
@@ -88,16 +105,26 @@ class MeshMaker:
         mesh.vertices = o3d.utility.Vector3dVector(vertices.reshape(-1, 3))
         mesh.triangles = o3d.utility.Vector3iVector(faces[:, 1:])
         mesh.vertex_colors = o3d.utility.Vector3dVector(textures.reshape(-1, 3))
+
+        ptc = o3d.geometry.PointCloud()
+        ptc.points  = o3d.utility.Vector3dVector(vertices[eroded_mask].reshape(-1, 3))
+        ptc.colors  = o3d.utility.Vector3dVector(textures[eroded_mask].reshape(-1, 3))
         
-        return mesh
+        return mesh, ptc
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--depth', type=str)
     parser.add_argument('--image', type=str)
+    parser.add_argument('--is_png', action='store_true', default=False)
     arg = parser.parse_args()
 
-    meshmaker = MeshMaker(arg.depth, arg.image)
-    mesh = meshmaker.make_textured_mesh()
+    meshmaker = MeshMaker(arg.depth, arg.image, is_png = arg.is_png)
+    mesh, ptc = meshmaker.make_textured_mesh()
 
-    o3d.io.write_triangle_mesh(arg.depth.replace('npy', 'ply'), mesh)
+    if arg.is_png:
+        o3d.io.write_point_cloud(arg.depth.replace('png', 'ply').replace('depth', 'ptc'), ptc)
+        o3d.io.write_triangle_mesh(arg.depth.replace('png', 'ply'), mesh)
+    else:
+        o3d.io.write_point_cloud(arg.depth.replace('npy', 'ply').replace('depth', 'ptc'), ptc)
+        o3d.io.write_triangle_mesh(arg.depth.replace('npy', 'ply'), mesh)
